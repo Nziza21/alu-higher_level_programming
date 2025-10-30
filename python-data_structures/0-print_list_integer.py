405af750272e3f7b86d6ff48a8c630d9207dd097
Nziza21 <n.samuel@alustudent.com>
2025-10-30 06:55:17 -0700

    update

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     """Print all integers of a list, one per line."""
-    for i in my_list:
+   if not isinstance(i, int):
         if type(i) != int:
             raise TypeError("All elements must be integers")
         print("{:d}".format(i))
