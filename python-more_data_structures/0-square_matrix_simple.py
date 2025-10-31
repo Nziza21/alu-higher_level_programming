63a0d9a2fdbe63bca0de1a5871746a8b0c8f675b
Nziza21 <n.samuel@alustudent.com>
2025-10-31 12:35:47 -0700

    completed

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """Computes the square of all integers in a 2D matrix."""
-    return [[value ** 2 for value in row] for row in matrix]
+    return list(map(lambda x: [y*y for y in x], matrix))
