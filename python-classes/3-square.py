f40283ecabf86c03f93fbaa0f6afe3987d06bb68
Nziza21 <n.samuel@alustudent.com>
2025-11-01 06:54:28 -0700

    square-3

---
+++ python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class Square with size validation and area computation."""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize the square with a given size.
+
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size * self.__size
