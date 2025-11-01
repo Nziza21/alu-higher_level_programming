8843ea6019e46562c4cf5a31b3b94c5d08fdf72c
Nziza21 <n.samuel@alustudent.com>
2025-11-01 07:40:22 -0700

    square-102

---
+++ python-classes/102-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Defines a class Square with size validation, area computation,
+and comparison operators based on area."""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, (int, float)):
+            raise TypeError("size must be a number")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
